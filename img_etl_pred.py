8b7b910d99e3c8112acbcef04e5df1dc357e845c
Mike Soukup <michaeltsoukup@gmail.com>
2023-08-12 17:30:50 -0500

    SVM on MNIST is ready for the app

--- img_etl_pred.py
+++ img_etl_pred.py
@@ -1,18 +1,17 @@
+import sklearn
+import pickle
 import numpy as np
 from PIL import Image
 
 def load_model(model_path: str):
     """Driver function to load in our model"""
-    model = keras.models.load_model(model_path)
+    model = pickle.load(open(model_path, 'rb'))
     return model
 
 def make_prediction(img_path):
     '''Driver funtion to make prediction on input Fashion MNIST image'''
-    ### Model Labels:
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
-    ### Load Fashion MNIST Model
-    model_path = r'./model/fashion_mnist.h5'
+    ### Load MNIST Model
+    model_path = r'./model/mnist_svm.pickle'
     model = load_model(model_path)
     ### Load in the image:
     with Image.open(img_path) as img:
@@ -20,7 +19,6 @@ def make_prediction(img_path):
     ### Convert to array:
     im_arr = np.asarray(img)
     im_arr = im_arr/255.0
-    im_arr = np.reshape(im_arr, (1,28,28))
-    pred = np.argmax(model.predict(im_arr), axis=-1)
-    pred = np.array(class_names)[pred][0]
+    im_arr = np.reshape(im_arr, (784,))
+    pred = model.predict([im_arr])[0]
     return pred
